cff65f8c0fcc4b827f007afb088378cce18dbc63
eduardoa2025-cyber <eduardoa.2025@alunos.utfpr.edu.br>
2026-05-28 20:22:23 -0300

    #BNVEMM001 - TESTEMAT - Criando um Painel para o Jogo.

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    printf("CCH ESW2 - Batalha Naval - Grupo EMM");
+    	
+    printf("1 = Novo Jogo\n 2 = Configurar\n 3 = Carregar ultimo Jogo\n 4 = Sair\n");
+			  
+		int n;
+		scanf("%d", &n);
+
+		    switch (n){
+			case 1:
+			printf("Novo Jogo\n");
+				break;
+            
+			case 2:
+			printf("Configurar\n");
+			    break;
+			case 3:
+			printf("Carregar ultimo jogo\n");
+			    break;
+			case 4:
+			printf("Sair\n");
+			
+			default:
+			printf("nenhuma opção encontrada\n");
+				break;
+			}
+    	
+
 }
