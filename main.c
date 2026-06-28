ed861a6eb0dc30ee0d4e1265cf8e73416efd5ba7
Marcos <marcosleandro@alunos.utfpr.edu.br>
2026-06-28 11:29:23 -0300

    Atualização: adicionado ataque do computador aleatorio e melhorando campo de visao

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void mostrarCampo(int **matriz, int tam) {
             else if(matriz[i][j] == NAVIO)
                 printf("O ");
             else if(matriz[i][j] == AGUADESCONHECIDA)
-                printf("? ");
+                printf("7 ");
             else if(matriz[i][j] == NAVIOATINGIDO)
                 printf("X ");
         }
@@ -111,7 +111,7 @@ void mostrarmatriz(int **matriz, int tam, const char *titulo){
     }
 }
 
-void atirar(int **campo, int **visao, int tam) {
+void atirarJogador(int **campo, int **visao, int tam) {
     int linha, coluna;
 
     printf("Digite linha e coluna: ");
@@ -126,105 +126,107 @@ void atirar(int **campo, int **visao, int tam) {
         visao[linha][coluna] = AGUA;
     }
 }
-int main(){
-    printf("1 = Novo Jogo\n 2 = Configurar\n 3 = Carregar ultimo Jogo\n 4 = Sair\n");
-			  
-		int n;
-		scanf("%d", &n);
+void ataqueComputador(int **campo, int **visao, int tam) {
+    int linha, coluna;
 
-		    switch (n){
-			case 1:
-			printf("Novo Jogo\n");
-				break;
-            
-			case 2:
-			printf("Configurar\n");
-			    break;
-			case 3:
-			printf("Carregar ultimo jogo\n");
-			    break;
-			case 4:
-			printf("Sair\n");
-			
-			default:
-			printf("nenhuma opção encontrada\n");
-				break;
-			}
-    	printf("teste Mateus 14/06");
-		printf("teste2 Mateus 14/06");
-	
+    while (1) {
+        linha = rand() % tam;
+        coluna = rand() % tam;
+
+        if (visao[linha][coluna] == AGUADESCONHECIDA) {
+            printf("\nComputador atirou em: %d %d\n", linha, coluna);
+
+            if (campo[linha][coluna] == NAVIO) {
+                printf("Computador acertou!\n");
+                campo[linha][coluna] = NAVIOATINGIDO;
+                visao[linha][coluna] = NAVIOATINGIDO;
+            } else {
+                printf("Computador errou!\n");
+                visao[linha][coluna] = AGUA;
+            }
+            break;
+        }
+    }
+}
+int main() {
+    int n;
+
+    srand(time(NULL));
+
     printf("1 = Novo Jogo\n");
     printf("2 = Configurar\n");
     printf("3 = Carregar ultimo Jogo\n");
     printf("4 = Sair\n");
-	srand(time(NULL));
-   
-    int **seu_campo = NULL;
-    int **campo_visao_adversario = NULL;
 
     scanf("%d", &n);
+
+    int **seu_campo = NULL;
+    int **campo_inimigo = NULL;
+    int **sua_visao = NULL;
+    int **visao_computador = NULL;
 
     switch(n){
 
         case 1:
-
             printf("Novo Jogo\n");
 
             seu_campo = AlocarMatriz(TAMANHO);
-            campo_visao_adversario = AlocarMatriz(TAMANHO);
+            campo_inimigo = AlocarMatriz(TAMANHO);
+            sua_visao = AlocarMatriz(TAMANHO);
+            visao_computador = AlocarMatriz(TAMANHO);
 
-			posicionarBarcos(seu_campo, TAMANHO);
+            posicionarBarcos(seu_campo, TAMANHO);
+            posicionarBarcos(campo_inimigo, TAMANHO);
 
-            if(campo_visao_adversario != NULL){
-                for(int i = 0; i < TAMANHO; i++){
-                    for(int j = 0; j < TAMANHO; j++){
-                        campo_visao_adversario[i][j] = AGUADESCONHECIDA;
-                    }
+            for(int i = 0; i < TAMANHO; i++){
+                for(int j = 0; j < TAMANHO; j++){
+                    sua_visao[i][j] = AGUADESCONHECIDA;
+                    visao_computador[i][j] = AGUADESCONHECIDA;
                 }
             }
 
-            if(seu_campo != NULL && campo_visao_adversario != NULL){
-
-                printf("\nMatrizes alocadas com sucesso!\n");
+            if(seu_campo && campo_inimigo && sua_visao && visao_computador){
 
                 printf("\nSEU CAMPO:\n");
-				mostrarCampo(seu_campo, TAMANHO);
-                printf("\nCAMPO DE VISAO DO ADVERSARIO:\n");
-				mostrarCampo(campo_visao_adversario, TAMANHO);
-				
-				for(int i = 0; i < 5; i++) {
-   				mostrarCampo(campo_visao_adversario, TAMANHO);
-  				atirar(seu_campo, campo_visao_adversario, TAMANHO);
-				}
+                mostrarCampo(seu_campo, TAMANHO);
+
+                for(int turno = 0; turno < 5; turno++) {
+
+                    printf("\n=== SUA VEZ ===\n");
+                    mostrarCampo(sua_visao, TAMANHO);
+                    atirarJogador(campo_inimigo, sua_visao, TAMANHO);
+
+                    printf("\n=== VEZ DO COMPUTADOR ===\n");
+                    ataqueComputador(seu_campo, visao_computador, TAMANHO);
+
+                    printf("\nSEU CAMPO ATUAL:\n");
+                    mostrarCampo(seu_campo, TAMANHO);
+                }
+
                 limpar_campo(seu_campo, TAMANHO);
-                limpar_campo(campo_visao_adversario, TAMANHO);
+                limpar_campo(campo_inimigo, TAMANHO);
+                limpar_campo(sua_visao, TAMANHO);
+                limpar_campo(visao_computador, TAMANHO);
             }
 
             break;
 
         case 2:
-
             printf("Configurar\n");
-
             break;
 
         case 3:
-
             printf("Carregar ultimo jogo\n");
-
             break;
 
         case 4:
-
             printf("Sair\n");
-
             break;
 
         default:
-
             printf("Nenhuma opcao encontrada.\n");
-
             break;
     }
+
     return 0;
 }
